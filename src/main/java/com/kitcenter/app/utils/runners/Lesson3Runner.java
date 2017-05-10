7a122940764ec14233df07503266cba5ef21e4b8
neporada <neporada.denis@gmail.com>
2017-05-10 10:12:52 +0300

    Homework for lesson 4

--- src/main/java/com/kitcenter/app/utils/runners/Lesson3Runner.java
+++ src/main/java/com/kitcenter/app/utils/runners/Lesson3Runner.java
@@ -1,8 +1,15 @@
 package com.kitcenter.app.utils.runners;
 
+import com.kitcenter.app.utils.convertors.PrimitiveConvertor;
+
 /**
  * Created by denis on 5/6/17.
  */
 public class Lesson3Runner {
-    public static void main (String args[])
+    public void main (String args[]){
+        PrimitiveConvertor myPC = new PrimitiveConvertor();
+        myPC.charToInt();
+        myPC.floatToChar();
+        myPC.intToChar();
+    }
 }
