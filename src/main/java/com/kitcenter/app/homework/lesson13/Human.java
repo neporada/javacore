1cef710842cb9c2fac5d0a34d57ba69e24626cd6
neporada <neporada.denis@gmail.com>
2017-06-13 11:02:19 +0300

    HomeWork #13 b)

--- src/main/java/com/kitcenter/app/homework/lesson13/Human.java
+++ src/main/java/com/kitcenter/app/homework/lesson13/Human.java
@@ -6,6 +6,14 @@ import java.util.Scanner;
  * Created by denis on 6/10/17.
  */
 public class Human {
+
+    // Написать класс Human который имеет только один конструктор (конструктор должен быть с параметрами,
+    // принимающий на входе возраст и имя человека). Поля возраст и имя должны быть приватными,
+    // а в классе должны быть реализованы геттеры getName(), getAge() и сеттер setAge (int age)
+    // который проверяет что возраст человека находится в диапазоне 1..120 лет, и в зависимости от этого
+    // выводит сообщение “New age setted” и сохраняет значение в поле возраста, или выводит сообщение “Age invalid”
+    // (если возраст не входит в диапазон).
+
     private String name;
     private int age;
 
@@ -24,5 +32,10 @@ public class Human {
         }else{
             System.out.println("“Age invalid”");
         }
+        // return ?;
+    }
+    public Human(String name, int age){
+        this.name = name;
+        this.age = age;
     }
 }
